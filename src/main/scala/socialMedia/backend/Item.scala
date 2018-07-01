98b20761f7dcd24c21d54bd2f1fdfa0d53d82304
NAEGELY Arthur <arthur.naegely@gmail.com>
2018-07-01 18:27:31 +0200

    [UPDATE] item to store, remove location field as it is an option

--- src/main/scala/socialMedia/backend/Item.scala
+++ src/main/scala/socialMedia/backend/Item.scala
@@ -3,12 +3,13 @@ package socialMedia.backend
 import org.joda.time.DateTime
 
 abstract class Item
-case class User(id: Int, name: String, location: Option[String], age: Int)
+case class User(id: Int, name: String, age: Int)
     extends Item
+
 case class Post(id: Int,
                 text: String,
                 fromUser: Int,
-                date: DateTime,
-                location: Option[String])
+                date: DateTime)
     extends Item
-case class Message(id: Int, fromUser: Int, toUser: Int, date: DateTime)
+
+case class Message(id: Int, fromUser: Int, toUser: Int, date: DateTime) extends Item
